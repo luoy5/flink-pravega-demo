4682a1581bc850ac1bcde45995ad11131d05ded1
anna-wang5 <anna_wang5@dell.com>
2021-08-17 21:42:56 -0400

    add deploy tips

--- start.sh
+++ start.sh
@@ -7,3 +7,7 @@ done
 java -cp $cpath:target/flink-pravega-demo-0.0.1-SNAPSHOT.jar $@
 #io.pravega.example.flink.demo.MyWriter
 #io.pravega.example.flink.demo.MyReader
+
+# before run you need to package and copy dependencies
+# mvn clean package
+# mvn dependency:copy-dependencies
